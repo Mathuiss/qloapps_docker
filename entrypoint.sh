90c061eba04454946dc2816eae12f2fe7f3f8d14
Mathuiss <mathijsgeerlings@gmail.com>
2023-09-24 16:43:18 +0200

    Credentials last

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-# Run the credentials script
-/etc/credentials.sh
-
 # Run db server
 service mysql start
 
 # Start web server
 service apache2 start
+
+# Run the credentials script
+/etc/credentials.sh
 
 # Log the errors of the container
 tail -f /var/log/apache2/*.log
